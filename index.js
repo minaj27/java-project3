c9b7675eab98f0ff6bab20058dea9d1932242647
minaj27 <minaj13m@gmail.com>
2023-12-04 23:50:06 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+var num1=25;
+var num2=225;
+
+document.write("<h1>External JS Demo</h1>")
+
+document.write("sum of <b>num1</b> and <b>num2</b> is:")
+
+var sum=num1+num2;
+
+document.write("<b>"+sum+"</b>")
